cdfc151dae412b58bcebc20abb6d220b4549f17c
Leandro Muller <vonleandromuller@gmail.com>
2016-11-20 23:20:45 +0000

    bugfix de callback do lastfm

--- app/controllers/user_configuration_controller.rb
+++ app/controllers/user_configuration_controller.rb
@@ -18,14 +18,14 @@ class UserConfigurationController < ApplicationController
   end
   
   def newlastfmaccountsync
-    @user = get_current_user
+    @user ||= get_current_user
     redirect_when_not_logged_in
   end
   
   def createlastfmaccountsync
-    @user = get_current_user
+    @user ||= get_current_user
     @user.update_attribute(:lastfm_username, params[:user][:lastfm_username])
-    redirect_to 'http://www.last.fm/api/auth/?api_key=5da1c00d06ef449b8c672b4be3ebda79&cb=' + request.base_url + root_url #url_for(:only_path => false) #request.base_url #criar helper para key api
+    redirect_to 'http://www.last.fm/api/auth/?api_key=5da1c00d06ef449b8c672b4be3ebda79&cb=' + request.base_url + mysettings_path #url_for(:only_path => false) #request.base_url #criar helper para key api
   end
   
 end
