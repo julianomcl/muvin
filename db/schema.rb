374ca46e84c618250ebfadca4a986166d080f768
Lucas Santana <lucassantana.402@gmail.com>
2016-11-27 18:34:12 +0000

    View updateemail

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20161124043201) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "lastfm_username"
-    t.string   "lastfm_token"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "activation_digest"
