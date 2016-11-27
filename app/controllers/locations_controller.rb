eb4ca8962c86b21310c430bc294714ee0bf8595d
Leandro Muller <vonleandromuller@gmail.com>
2016-11-27 18:12:37 +0000

    buscar musicas com api do lastfm

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
   include SessionsHelper
+  include LastfmHelper
 
   def index
     @locations = Location.all
@@ -12,10 +13,23 @@ class LocationsController < ApplicationController
   def create
     if is_user_logged_in?
       @location = Location.new(location_params)
+      user = get_current_user
+      curloc = Location.where("latitude - #{@location.latitude} < 0.1 AND longitude - #{@location.longitude} < 0.1 AND user_id = #{user.id}").first
+      if !curloc.nil? then
+        @location = curloc
+      end
       respond_to do |format|
         if @location.save
           format.json { render :show, status: :created, location: @location }
           format.js {}
+          recenttracks = get_recent_tracks(user.id, 15, @location)
+          recenttracks.each do |index, track|
+            music = Music.new
+            music.artist_name = track['artist']['#text']
+            music.location_id = @location.id
+            music.track_name = track['name']
+            music.save
+          end
         else
           format.json { render json: @location.errors, status: :unprocessable_entity }
           format.js {}
@@ -33,5 +47,4 @@ class LocationsController < ApplicationController
     def location_params
       params.require(:location).permit(:latitude, :longitude).merge(:user_id => session[:user_id])
     end
-
 end
