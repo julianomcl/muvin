7a93a7b4d181cb3075cd59f7001a5299a6c0fa2d
Juliano Maia <leite.juliano@gmail.com>
2016-11-28 19:48:01 -0200

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20161127163939) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "lastfm_username"
+    t.string   "lastfm_token"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "activation_digest"
