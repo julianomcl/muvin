aef305677778563b8d35e642aa4146665af4de3d
Juliano Maia <leite.juliano@gmail.com>
2016-11-30 01:42:01 -0200

    remove redirect to spotify auth

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,27 +2,20 @@ class WelcomeController < ApplicationController
   include LastfmHelper
   
   def index
+    @spotify_musics = []
     if is_user_logged_in?
-      if session[:hash] == nil
-        redirect_to '/auth/spotify'
-      else
-        @spotify_musics = []
-        location = Location.find_by(:user_id => session[:user_id])
-        if location != nil
-          @locations = nil
-          @locations = Location.get_by_distance(location, 1)
-          @locations = Location.get_by_distance(location, 10) if @locations.blank?
-          @spotify_user = RSpotify::User.new(session[:hash])
-          musics = Music.get_musics_by_location(location)
-          
-          musics.each do |music|
-            m = RSpotify::Track.search(music.track_name).first
-            @spotify_musics.push(m) unless m.nil?
-          end
+      location = Location.find_by(:user_id => session[:user_id])
+      if location != nil
+        @locations = nil
+        @locations = Location.get_by_distance(location, 1)
+        @locations = Location.get_by_distance(location, 10) if @locations.blank?
+        musics = Music.get_musics_by_location(location)
+
+        musics.each do |music|
+          m = RSpotify::Track.search(music.track_name).first
+          @spotify_musics.push(m) unless m.nil?
         end
       end
-    else
-      @spotify_musics = []
     end
   end
 end
